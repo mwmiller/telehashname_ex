d87f040f8a2089323ec51b6f4c852909e1c320c4
Matt Miller <mwm@fastmail.com>
2016-02-27 15:15:24 +0200

    Add `order_valid_cs_pairs`.
    Not the world's best name, but I can revisit.  Regardless, this sets in
    motion the plan, which is to silently drop invalid CSIDs and hash up the
    rest.
    
    Truly interested parties can look at the map to see got through and
    contributed.
    
    Also applies a convenience conversion of "cs"- IDs.  Not so hep on the
    magic, but meh.

--- lib/telehashname.ex
+++ lib/telehashname.ex
@@ -4,9 +4,21 @@ defmodule Telehashname do
   @type csk :: binary
   @type cs_pair :: {csid, csk}
 
-  @spec from_tuples([cs_pair]) :: {binary, map}
+  @spec from_tuples([cs_pair]) :: {binary, map} | nil
   def from_tuples(csk_list) do
-    csk_list |> Enum.sort(&(elem(&1,0) <= elem(&2,0))) |> hash_tuples({"",%{}})
+    csk_list |> order_valid_cs_pairs |> hash_tuples({"",%{}})
+  end
+
+  def order_valid_cs_pairs(list), do: ovcp(list, [])
+  defp ovcp([], acc), do: acc |> Enum.sort(&(elem(&1,0) <= elem(&2,0)))
+  defp ovcp([{csid, csk}|rest],acc) do
+    csid = if byte_size(csid) == 4 and binary_part(csid, 0,2) == "cs", do: binary_part(csid,2,2), else: csid
+    newacc = if is_valid_csid? csid do
+        [{csid,csk}|acc]
+    else
+        acc
+    end
+    ovcp(rest, newacc)
   end
 
   @spec is_valid_csid?(term) :: boolean
@@ -20,7 +32,8 @@ defmodule Telehashname do
 
   defp bp, do: [case: :lower, padding: false]
 
-  defp hash_tuples([], {h,m}), do: {h |> Base.encode32(bp), m}
+  defp hash_tuples([], {h,m}) when byte_size(h) > 0, do: {h |> Base.encode32(bp), m}
+  defp hash_tuples([], _empty_tuple),  do: nil
   defp hash_tuples([{csid, csk}|rest], {h,m}) do
     hash = :crypto.hash(:sha256, h<>Base.decode16!(csid, bp))
     intr = :crypto.hash(:sha256, Base.decode32!(csk, bp))
--- test/telehashname_test.exs
+++ test/telehashname_test.exs
@@ -4,21 +4,29 @@ defmodule TelehashnameTest do
   alias Telehashname, as: Hashname
 
   test "from tuples" do
-  csk_list = [  {"3a","hp6yglmmqwcbw5hno37uauh6fn6dx5oj7s5vtapaifrur2jv6zha"}, {"1a","vgjz3yjb6cevxjomdleilmzasbj6lcc7"} ]
-  assert Hashname.from_tuples(csk_list) == {"jvdoio6kjvf3yqnxfvck43twaibbg4pmb7y3mqnvxafb26rqllwa",
-                                                %{"1a" => "ym7p66flpzyncnwkzxv2qk5dtosgnnstgfhw6xj2wvbvm7oz5oaq",
-                                                  "3a" => "bmxelsxgecormqjlnati6chxqua7wzipxliw5le35ifwxlge2zva"}
-                                               }
-  csk_list = [ {"3a","eg3fxjnjkz763cjfnhyabeftyf75m2s4gll3gvmuacegax5h6nia"}, {"1a", "an7lbl5e6vk4ql6nblznjicn5rmf3lmzlm"} ]
-  assert Hashname.from_tuples(csk_list) == {"27ywx5e5ylzxfzxrhptowvwntqrd3jhksyxrfkzi6jfn64d3lwxa",
-                                                %{"1a" => "eg3fxjnjkz763cjfnhyabeftyf75m2s4gll3gvmuacegax5h6nia",
-                                                  "3a" => "s7md2gxysgmhjjcjo2iuln5tznddlgzmcilj5zj6na2hppweoeaq"}
-                                               }
+    csk_list = [  {"3a","hp6yglmmqwcbw5hno37uauh6fn6dx5oj7s5vtapaifrur2jv6zha"}, {"1a","vgjz3yjb6cevxjomdleilmzasbj6lcc7"} ]
+    assert Hashname.from_tuples(csk_list) == {"jvdoio6kjvf3yqnxfvck43twaibbg4pmb7y3mqnvxafb26rqllwa",
+                                              %{"1a" => "ym7p66flpzyncnwkzxv2qk5dtosgnnstgfhw6xj2wvbvm7oz5oaq",
+                                                "3a" => "bmxelsxgecormqjlnati6chxqua7wzipxliw5le35ifwxlge2zva"}
+                                             }
+    csk_list = [ {"3a","eg3fxjnjkz763cjfnhyabeftyf75m2s4gll3gvmuacegax5h6nia"}, {"1a", "an7lbl5e6vk4ql6nblznjicn5rmf3lmzlm"} ]
+    assert Hashname.from_tuples(csk_list) == {"27ywx5e5ylzxfzxrhptowvwntqrd3jhksyxrfkzi6jfn64d3lwxa",
+                                              %{"1a" => "eg3fxjnjkz763cjfnhyabeftyf75m2s4gll3gvmuacegax5h6nia",
+                                                "3a" => "s7md2gxysgmhjjcjo2iuln5tznddlgzmcilj5zj6na2hppweoeaq"}
+                                             }
 
-  csk_list = [{"1a", "vgjz3yjb6cevxjomdleilmzasbj6lcc7"}]
-  assert Hashname.from_tuples(csk_list) == {"echmb6eke2f6z2mqdwifrt6i6hkkfua7hiisgrms6pwttd6jubiq",
-                                                %{"1a" => "ym7p66flpzyncnwkzxv2qk5dtosgnnstgfhw6xj2wvbvm7oz5oaq"}
-                                               }
+    csk_list = [{"cs1a", "vgjz3yjb6cevxjomdleilmzasbj6lcc7"}]
+    assert Hashname.from_tuples(csk_list) == {"echmb6eke2f6z2mqdwifrt6i6hkkfua7hiisgrms6pwttd6jubiq",
+                                               %{"1a" => "ym7p66flpzyncnwkzxv2qk5dtosgnnstgfhw6xj2wvbvm7oz5oaq"}
+                                             }
+
+    csk_list = [{"bad", "mojo"}]
+    assert Hashname.from_tuples(csk_list) == nil
+
+    csk_list = [{"cs1a", "vgjz3yjb6cevxjomdleilmzasbj6lcc7"}, {"bad", "mojo"}]
+    assert Hashname.from_tuples(csk_list) == {"echmb6eke2f6z2mqdwifrt6i6hkkfua7hiisgrms6pwttd6jubiq",
+                                              %{"1a" => "ym7p66flpzyncnwkzxv2qk5dtosgnnstgfhw6xj2wvbvm7oz5oaq"}
+                                             }
   end
 
   test "is_valid_csid?" do
@@ -26,6 +34,11 @@ defmodule TelehashnameTest do
     assert Hashname.is_valid_csid?("8a")
     refute Hashname.is_valid_csid?("at")
     refute Hashname.is_valid_csid?("bad")
+  end
+
+  test "order_valid_cs_pairs" do
+    in_list = [{"8a", "doot"}, {"cs1a", "root"}, {"toot", "2a"}, {"3a", "loot"}]
+    assert Hashname.order_valid_cs_pairs(in_list) == [{"1a", "root"}, {"3a", "loot"}, {"8a", "doot"}]
   end
 
 end
